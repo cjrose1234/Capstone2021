e8becff308e2a748825fc1c273bce6ac4ca3a7be
cjrose1234 <rosenbcs@dukes.jmu.edu>
2021-10-18 16:04:39 -0400

    .

---
+++ Capstone/addAffiliation.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/UImasterform.Master" AutoEventWireup="true" CodeBehind="addAffiliation.aspx.cs" Inherits="Capstone.addAffiliation" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderMainPage" runat="server">
+    <asp:Label ID="lblTitle" runat="server" Text="Add Affiliations" Font-Size="X-Large"></asp:Label>
+    <br />
+    <br />
+    <br />
+    <asp:Label ID="lblOrganizations" runat="server" Text="Organizations:"></asp:Label>
+    <br />
+    <asp:DropDownList ID="lstOrganizations" runat="server" Width="300"></asp:DropDownList>
+    <br />
+    <asp:Button ID="btnJoinOrg" runat="server" Text="Request to join" OnClick="btnJoinOrg_Click"/>
+</asp:Content>
---
+++ Capstone/addAffiliation.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Capstone
+{
+    public partial class addAffiliation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+
+                //establish connection to database
+                SqlConnection sqlCon = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString.ToString());
+
+                string sqlQuery = "SELECT orgName, orgID FROM Organizations";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, sqlCon);
+
+                sqlCon.Open();
+
+                SqlDataReader results = cmd.ExecuteReader();
+                while ( results.Read() )
+                {
+                    lstOrganizations.Items.Add(new ListItem(results["orgName"].ToString(), results["orgID"].ToString()));
+                }
+                sqlCon.Close();
+            }
+        }
+
+        protected void btnJoinOrg_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
